179424a176400d7bd05898cfc195d1804a55c03b
chae2 <saintcharry22@gmail.com>
2024-12-17 00:38:43 +0900

    데이터베이스 연동

--- type_client.py
+++ type_client.py
@@ -1,27 +1,29 @@
 import socket
 import time
+import mysql.connector
 
-HOST = '172.20.4.31' # socket.gethostbyname(socket.gethostname()) #'SERVER_IP'  # 서버 IP 주소 입력
+HOST = '172.20.34.67' # socket.gethostbyname(socket.gethostname()) #'SERVER_IP'  # 서버 IP 주소 입력
 PORT = 9999  # 서버 포트 번호
 
 # 서버와의 연결 설정
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-# 데이터베이스에서 가져온 문장 목록
-from_db = [
-    "가","나","다","라","마","바","사","아","자","차","카","타","파","하"
-    # "체다치즈를 최고 많이 먹은 최다연이 체다치즈 먹기 대회 최다 우승자이다.",
-    # "정희수가 희희낙락하게 희끄무리한 흰머리를 뽑으며",
-    # "이수지가 저수지에 갔는데 이 수지가 저수지에 간 걸까 저 수지가 저수지에 간걸까",
-    # "그 수지가 저수지에 간 걸까 하며 이수지는 고민했는데 고민 끝에 이수의 마이웨이를 부르며 불쾌지수가 올라가며",
-    # "저수지를 떠나 경기도 수지구의 한 학원으로 달려가더니 지수함수를 배워서 잘 사용하여 주식 수지를 맞아",
-    # "'나 이수지, 바로 고단수지! 수지맞았다!'하며 행복해했다.",
-    # "내가 그린 기린 그림은 잘 그린 기린 그림이고 네가 그린 기린 그림은 못 그린 기린 그림이다.",
-    # "내가 그린 기린 그림은 목이 긴 기린 그린 그림이고, 네가 그린 기린 그림은 목이 안 긴 기린 그린 그림이다",
-    # "내가 그린 구름그림은 새털구름 그린 구름그림이고, 네가 그린 구름그림은 깃털구름 그린 구름그림이다.",
-    # "저기 계신 저 분이 박 법학박사이시고 여기 계신 이 분이 백 법학박사이시다."
-]
+def get_random_sentence(): # 데이터베이스에서 문장 가져옴
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="cloud4",
+        password="qwerty",
+        database="typeDB"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT sentence FROM sentences ORDER BY RAND() LIMIT 10;")
+    sentences = [row[0] for row in cursor.fetchall()]
+
+    cursor.close()
+    connection.close()
+
+    return sentences
 
 def wait_for_match():
     '''대기상태로 돌아가 매칭신호 기다림'''
@@ -35,6 +37,7 @@ def wait_for_match():
 
 def play_game():
     '''타이핑 게임 실행하고 결과 서버 전송'''
+    sentences = get_random_sentence()
     player = client_socket.recv(1024).decode()
     print(f"Typing Game start! You are {player}.")
 
@@ -43,7 +46,7 @@ def play_game():
     for i in range(3):  # 10회의 타이핑 게임 진행
         same = False
         while not same:
-            compare = from_db[i]
+            compare = sentences[i]
             print(f"문장을 입력하세요: {compare}")
             from_user = input("입력: ")
             if from_user == compare:
